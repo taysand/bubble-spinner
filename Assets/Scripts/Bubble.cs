d6ba55e0596e626d404748497c9e25c2d9966d55
Taylor Sandusky <tsandusky@mills.edu>
2018-03-23 19:39:17 -0700

    Add bubble prefab Bubbles are given a random color on start

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+
+public class Bubble : MonoBehaviour {
+	public enum BubbleColor {Blue, Green, Red, Purple};
+	private int numBubbleColors;
+	private static Sprite[] sprites;
+	private const string bubbleSpriteFile = "bubbles";
+
+	private BubbleColor color;
+	
+	void Start () {
+		numBubbleColors = Enum.GetNames(typeof(BubbleColor)).Length;
+		color = (BubbleColor) Enum.ToObject(typeof(BubbleColor), UnityEngine.Random.Range (0, numBubbleColors));
+		sprites = Resources.LoadAll<Sprite> (bubbleSpriteFile);
+		GetComponent<SpriteRenderer> ().sprite = sprites[(int) color];
+	}
+	
+	void Update () {
+		
+	}
+
+	public BubbleColor GetColor() {
+		return color;
+	}
+}
