e5c74177a3cdca9d494483b88f7ffacac09f859b
Taylor Sandusky <tsandusky@mills.edu>
2018-03-23 20:28:24 -0700

    Begin adding physics. Bubble gravity can be turned on and off. Bubbles rotate around center circle. Center circle has reasonable rotation.

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -4,16 +4,18 @@ using System;
 public class Bubble : MonoBehaviour {
 	public enum BubbleColor {Blue, Green, Red, Purple};
 	private int numBubbleColors;
-	private static Sprite[] sprites;
+	private static Sprite[] bubbleSprites;
 	private const string bubbleSpriteFile = "bubbles";
 
 	private BubbleColor color;
+	public bool grouped = true;
 	
 	void Start () {
 		numBubbleColors = Enum.GetNames(typeof(BubbleColor)).Length;
 		color = (BubbleColor) Enum.ToObject(typeof(BubbleColor), UnityEngine.Random.Range (0, numBubbleColors));
-		sprites = Resources.LoadAll<Sprite> (bubbleSpriteFile);
-		GetComponent<SpriteRenderer> ().sprite = sprites[(int) color];
+		bubbleSprites = Resources.LoadAll<Sprite> (bubbleSpriteFile);
+		GetComponent<SpriteRenderer> ().sprite = bubbleSprites[(int) color];
+		// SwapGravity();
 	}
 	
 	void Update () {
@@ -22,5 +24,13 @@ public class Bubble : MonoBehaviour {
 
 	public BubbleColor GetColor() {
 		return color;
+	}
+
+	private void SwapGravity() {
+		if (grouped) {
+			GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+		} else {
+			GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+		}
 	}
 }
