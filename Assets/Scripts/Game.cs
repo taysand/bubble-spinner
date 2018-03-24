1532a5677481b67459f202208796739a3395564f
Taylor Sandusky <tsandusky@mills.edu>
2018-03-23 20:47:29 -0700

    Add walls

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Game : MonoBehaviour {
 
 	void Start () {
-		GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+		// GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
 	}
 	
 	void Update () {
