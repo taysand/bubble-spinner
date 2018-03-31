c1bba6651c9abd42e92323ba9dc9072881886f28
Taylor Sandusky <tsandusky@mills.edu>
2018-03-31 10:24:10 -0700

    Create multiple rows of bubbles

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,30 +6,39 @@ public class Game : MonoBehaviour {
 
 	public Bubble bubblePrefab;
 	public GameObject mass;
-	private int numBubbles = 5;
+
+	private int[] numBubblesPerRow = new int[] { 6, 12, 19 };
+	private int rowCount = 0;
+	private float radius;
 
 	void Start () {
 		// GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
-		StartLevel();
+		radius = Bubble.GetDiameter ();
+		StartLevel ();
 	}
-	
+
 	void Update () {
-		
+
 	}
 
-	private void StartLevel() {
-		for (int i = 0; i < numBubbles; i++) {
-			float theta = i * 2 * Mathf.PI / numBubbles;
-			float x = Mathf.Sin(theta) * Bubble.GetDiameter();
-			float y = Mathf.Cos(theta) * Bubble.GetDiameter();
+	private void StartLevel () {
+		while (rowCount < numBubblesPerRow.Length) {
+			for (int i = 0; i < numBubblesPerRow[rowCount]; i++) {
+				//https://www.reddit.com/r/Unity2D/comments/2rrapx/instantiating_multiple_objects_in_a_circle/
+				float theta = i * 2 * Mathf.PI / numBubblesPerRow[rowCount];
+				float x = Mathf.Sin (theta) * radius;
+				float y = Mathf.Cos (theta) * radius;
 
-			Bubble bubble = Instantiate(bubblePrefab) as Bubble;
-			HingeJoint2D joint = bubble.GetComponent<HingeJoint2D>();
-			joint.connectedBody = mass.GetComponent<Rigidbody2D>();
-			joint.connectedAnchor = new Vector2(0, 0);
-			bubble.transform.parent = mass.transform;
+				Bubble bubble = Instantiate (bubblePrefab) as Bubble;
+				HingeJoint2D joint = bubble.GetComponent<HingeJoint2D> ();
+				joint.connectedBody = mass.GetComponent<Rigidbody2D> ();
+				joint.connectedAnchor = new Vector2 (0, 0);
+				bubble.transform.parent = mass.transform;
 
-			bubble.transform.position = new Vector3(x, y, 0);
+				bubble.transform.position = new Vector3 (x, y, 0);
+			}
+			rowCount++;
+			radius += Bubble.GetDiameter ();
 		}
 	}
 }
