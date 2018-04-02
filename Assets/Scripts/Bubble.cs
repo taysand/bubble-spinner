a7f4f7cb1534beb9c20fc01b9197de12c393eece
Taylor Sandusky <tsandusky@mills.edu>
2018-04-02 10:19:41 -0700

    Add bubbles to center mass when they collide

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -1,9 +1,9 @@
-using UnityEngine;
-using System;
+﻿using System;
 using System.Collections;
+using UnityEngine;
 
 public class Bubble : MonoBehaviour {
-	public enum BubbleColor {Blue, Green, Red, Purple};
+	public enum BubbleColor { Blue, Green, Red, Purple };
 	private int numBubbleColors;
 	private static Sprite[] bubbleSprites;
 	private const string bubbleSpriteFile = "bubbles";
@@ -20,33 +20,29 @@ public class Bubble : MonoBehaviour {
 	}
 
 	void Start () {
-		numBubbleColors = Enum.GetNames(typeof(BubbleColor)).Length;
-		color = (BubbleColor) Enum.ToObject(typeof(BubbleColor), UnityEngine.Random.Range (0, numBubbleColors));
+		// Debug.Log ("bubble start");
+		numBubbleColors = Enum.GetNames (typeof (BubbleColor)).Length;
+		color = (BubbleColor) Enum.ToObject (typeof (BubbleColor), UnityEngine.Random.Range (0, numBubbleColors));
 		bubbleSprites = Resources.LoadAll<Sprite> (bubbleSpriteFile);
 		GetComponent<SpriteRenderer> ().sprite = bubbleSprites[(int) color];
-	}
-	
-	void Update () {
-		if (leaving) {
-			Leave();
+		if (!grouped) {
+			joint.enabled = false;
 		}
 	}
 
-	public BubbleColor GetColor() {
+	void Update () {
+		if (leaving) {
+			Leave ();
+		}
+	}
+
+	public BubbleColor GetColor () {
 		return color;
 	}
 
-	private void Leave() {
-		GetComponent<CircleCollider2D>().enabled = false;
-		GetComponent<HingeJoint2D>().enabled = false;
-		leaving = false;
-		Destroy(gameObject, 3f); //TODO: object pool
-	}
-
-	public static float GetDiameter() {
-		return diameter;
-	}
-}
+	private void Leave () {
+		GetComponent<CircleCollider2D> ().enabled = false;
+		GetComponent<HingeJoint2D> ().enabled = false;
 		leaving = false;
 		Destroy (gameObject, 3f); //TODO: object pool
 	}
@@ -57,10 +53,7 @@ public class Bubble : MonoBehaviour {
 
 	void OnCollisionEnter2D (Collision2D other) {
 		if (other.gameObject.GetComponent<Bubble> () != null) {
-			// Debug.Log ("hit bubble");
-			// Debug.Log ("grouped is " + grouped + " and connected body is " + GetComponent<HingeJoint2D> ().connectedBody);
-			grouped = true;
-
+			AddToMass();
 		}
 	}
 
