e29b732d399d279115cc2a861fc826d98d884d5a
Taylor Sandusky <tsandusky@mills.edu>
2018-04-20 18:48:08 -0700

    Have shooter look at mouse

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,11 +6,14 @@ public class Shooter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void Update () {
+		//https://forum.unity.com/threads/2d-rotating-object-based-on-mouse-position.248167/
+		Vector3 position = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 direction = Input.mousePosition - position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 }
